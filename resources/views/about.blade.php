2ad158186018524658e931734acb43d4b66b64aa
Autumn Charles <autumn.m.s.c@gmail.com>
2025-02-10 21:45:12 -0500

    completed exercise 6 add some css and an image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,4 +3,5 @@
 @section('content')
 	<h2>Autumn Charles</h2>
 	<p>I am currently a student at the UWI Five Islands Campus. I am having lots and lots of fun :D</p>
+	<img src="{{ url('/images/new world.png') }}" alt="My Image" />
 @endsection
